64027346beacb04c16c4a34bbe11d11913b8221b
carloseduneves <carlosneves.edu@gmail.com>
2021-12-26 18:49:31 -0300

    Calculadora de IMC 1.2

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+console.log('o console funcionou corretamente.')
+var vel = 40
+if(vel > 60){
+    console.log('Você ultrapassou o limite de velocidade permitida, multado!') //condição simples.
+}
+
+console.log(`A velocidade do seu carro é igual a ${vel}km por hora.`)
+console.log(`Use cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+if(país == 'Brasil'){
+    console.log('brasileiro')
+}
+else{
+    console.log('estrangeiro')
+}
